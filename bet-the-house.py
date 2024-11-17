369a5309e655aa14e3e0b2d2674945681514b795
salmoncave <seanm7.murphy@gmail.com>
2024-11-17 16:10:41 -0600

    Added card shoe and potential necessary functions, random card refactored into draw cards on shoe class

--- bet-the-house.py
+++ bet-the-house.py
@@ -12,8 +12,6 @@ class CardSuit(Enum):
 class PlayingCard():
 
     def __init__(self, suit: str, card_type: str):
-        card_types = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","Jack","Queen","King","Ace"]
-        card_suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
         self.suit : str = suit.lower().capitalize()
         self.type : str = card_type
@@ -21,7 +19,7 @@ class PlayingCard():
         self.name : str = (f"{self.type} of {self.suit}")
 
     def _calculate_card_value(self, card_type: str):
-        face_cards = ["Jack", "Queen", "King"]
+        face_cards : list[str] = ["Jack", "Queen", "King"]
         if card_type == "Ace":
             return 11
 ## Ace value set to 11 for now, TODO: add input to choose 1 or 11 after game structure implemented
@@ -30,34 +28,80 @@ class PlayingCard():
         else:
             return 10
 
+#Generates standard deck
 class StandardDeck():
     
     def __init__(self):
         
-        self.deck : list = self._generate_standard_deck()
-        
+        self.deck : list[PlayingCard] = self._generate_standard_deck()
     
     def _generate_standard_deck(self):
-        card_types = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","Jack","Queen","King","Ace"]
-        card_suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-        playing_deck : list = []
-        test_card_names: list = []
+        card_types : list[str] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","Jack","Queen","King","Ace"]
+        card_suits : list[str] = ["Clubs", "Diamonds", "Hearts", "Spades"]
+        playing_deck : list[PlayingCard] = []
 
         for suit in card_suits:
             for type in card_types:
                 card = PlayingCard(suit= suit, card_type= type)
                 playing_deck.append(card)
-                test_card_names.append(card.name)
-        return test_card_names
+       
+        return playing_deck
+
+class DealingShoe():
+
+    def __init__(self):
+
+        self.total_decks : int = 3
+        self.card_shoe : list[PlayingCard] = self._generate_card_shoe(self.total_decks)
+
+    def _generate_card_shoe(self, decks: int):
+        shoe : list[PlayingCard] = []
+        
+        for deck in range(decks):
+            shoe.extend(StandardDeck().deck)
+        
+        return shoe
     
-    def pick_random_card(self):
-        random_card = random.choice(self.deck)
-        self.deck.remove(random_card)
-    
+    def draw_cards_from_shoe(self, cards_to_draw : int = 1):
+        drawn_cards : list[PlayingCard] = []
+        
+        for cards in range(cards_to_draw):
+            random_card : PlayingCard = random.choice(self.card_shoe)
+            
+            drawn_cards.append(random_card)
+            self.card_shoe.remove(random_card)
+        
+        return drawn_cards
+
+    def draw_full_shoe(self):
+        pass
+
+    def _regenerate_shoe(self, regen_threshold: int = 25):
+        pass
+
+    def interpret_cards_to_text(self, cards : list[PlayingCard]):
+        interpreted_cards : list[str] = []
+        
+        for card in cards:
+            interpreted_cards.append(card.name)
+
+        return interpreted_cards
+
+
+## Starts gameplay structure, called by main
+def play_game():
+    ''' testdeck = StandardDeck()
+    print_range = range(26)
+    for i in print_range:
+        print(testdeck.pick_random_card().name)
+    print("---------------------------------------------------")
+    for n in print_range:
+        print(testdeck.pick_random_card().name) '''
+    testshoe = DealingShoe()
+    pprint.pprint(testshoe.interpret_cards_to_text(testshoe.draw_cards_from_shoe(5)))
 
 def main():
-    testdeck = StandardDeck
-    pprint.pprint(testdeck._generate_standard_deck(testdeck))
+    play_game()
 
 if __name__ == "__main__":
     main()
